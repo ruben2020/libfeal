9ade027c273964e2ab9b15ae8b0e7729d578b616
ruben2020 <ruben2020-git@users.sourceforge.net>
2021-10-28 01:49:48 +0800

    Changed main license to MPL2, while example code is licensed under MIT license

--- include/internal/FealCommon_posix.h
+++ include/internal/FealCommon_posix.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020 ruben2020 https://github.com/ruben2020
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 #ifndef _FEAL_COMMON_POSIX_H
 #define _FEAL_COMMON_POSIX_H
 
--- src/FealBaseStream_kqueue.cpp
+++ src/FealBaseStream_kqueue.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020 ruben2020 https://github.com/ruben2020
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 #include "feal.h"
 
 int  feal::BaseStream::accept_new_conn(void){return FEAL_SOCKET_ERROR;}
