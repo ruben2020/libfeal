56e4079b3c1a42ee5b6ec0758cd6b67915a9020f
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-04-20 00:17:03 +0800

    Updated LICENSE

--- src/FealEvent.cpp
+++ src/FealEvent.cpp
@@ -1,12 +1,24 @@
 /*
- * Copyright (c) 2020 ruben2020 https://github.com/ruben2020
+ * Copyright 2021 ruben2020 https://github.com/ruben2020
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Dual-licensed under the Apache License, Version 2.0, and
+ * the GNU General Public License, Version 2.0;
+ * you may not use this file except in compliance
+ * with either one of these licenses.
+ *
+ * You can find copies of these licenses in the included
+ * LICENSE-APACHE2 and LICENSE-GPL2 files.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under these licenses is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the license texts for the specific language governing permissions
+ * and limitations under the licenses.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-only
  *
  */
-
+ 
 #include "feal.h"
 
 feal::EventId_t feal::Event::getId(void)
