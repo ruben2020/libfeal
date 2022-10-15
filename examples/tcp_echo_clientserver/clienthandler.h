9f10489f487941e4dc259ef735a9a4b9c627d052
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-10-15 18:14:00 +0800

    Updated license in comments

--- examples/tcp_echo_clientserver/clienthandler.h
+++ examples/tcp_echo_clientserver/clienthandler.h
@@ -1,20 +1,7 @@
-/*
- * Copyright 2021 ruben2020 https://github.com/ruben2020
- *
- * Licensed under the Apache License, Version 2.0
- * with LLVM Exceptions (the "License");
- * you may not use this file except in compliance with the License.
- * You can find a copy of the License in the LICENSE file.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
- * 
- */
+//
+// Copyright (c) 2022 ruben2020 https://github.com/ruben2020
+// SPDX-License-Identifier: MIT
+//
  
 #ifndef _CLIENTHANDLER_H
 #define _CLIENTHANDLER_H
