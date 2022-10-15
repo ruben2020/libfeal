50b3adbb159dab70b22adaf8609710d87dbf8a3c
ruben2020 <ruben2020-git@users.sourceforge.net>
2022-10-15 19:00:59 +0800

    abstract Unix socket example added

---
+++ examples/uns_echo_clientserver_abstract/clientuns.cpp
@@ -0,0 +1,27 @@
+//
+// Copyright (c) 2022 ruben2020 https://github.com/ruben2020
+// SPDX-License-Identifier: MIT
+//
+ 
+#include <cstdio>
+#include <cstring>
+#include "clientuns.h"
+
+#define SERVERPATH "fealunsserver"
+
+void Clientuns::connect_to_server(void)
+{
+    struct sockaddr_un serveraddr;
+    serveraddr.sun_family = AF_UNIX;
+    serveraddr.sun_path[0] = 0;
+    strcpy(serveraddr.sun_path + 1, SERVERPATH);
+    printf("Trying to connect %s\n", SERVERPATH);
+    feal::errenum se = stream.create_and_connect(&serveraddr);
+    if (se != feal::FEAL_OK)
+    {
+        printf("Error connecting to %s  err %d\n", SERVERPATH, se);
+        timers.startTimer<EvtRetryTimer>(std::chrono::seconds(5));
+    }
+}
+
+
---
+++ examples/uns_echo_clientserver_abstract/clientuns.h
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2022 ruben2020 https://github.com/ruben2020
+// SPDX-License-Identifier: MIT
+//
+ 
+#ifndef _CLIENTUNS_H
+#define _CLIENTUNS_H
+
+#include "feal.h"
+#include "client.h"
+
+class Clientuns : public Client
+{
+
+public:
+
+Clientuns() = default;
+~Clientuns() = default;
+
+
+protected:
+
+void connect_to_server(void);
+
+};
+
+#endif // _CLIENTUNS_H
+
